203d59362b261d829d8eebe77bbb90df70c4e162
Manu Sporny <msporny@digitalbazaar.com>
2011-07-31 15:27:53 -0400

    Added dial-in instructions.

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ print <<< htmlcode
    xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
    xmlns:dcterms="http://purl.org/dc/terms/"
    xmlns:vcard="http://www.w3.org/2006/vcard/ns#"
-   xmlns:v="http://rdf.data-vocabulary.org/#"
-   > 
+   xmlns:v="http://rdf.data-vocabulary.org/#"> 
    <head> 
       <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
       <title>JSON-LD - Teleconference Logs</title> 
@@ -29,6 +28,20 @@ print <<< htmlcode
          <div id="content"> 
             <div><a href="../">JSON-LD</a> &gt; Teleconference Logs</div>
             <div id="info"> 
+               <h1>Joining Teleconferences</h1>
+               
+               <p>All JSON-LD teleconferences are open to the public. Anyone may join and participate in the discussion. All teleconferences are announced at least 24 hours in advance on the <a href="http://lists.w3.org/Archives/Public/public-linked-json/">JSON-LD mailing list</a>.</p>
+
+               <ul>
+                  <li><strong>When:</strong> Bi-monthly, on Tuesdays - second and fourth week of every month.</li>
+                  <li><strong>Time:</strong> 1500 UTC, 8am San Francisco, 11am Boston, 4pm London</li>
+                  <li><strong>Where:</strong> Digital Bazaar JSON-LD Telecon Bridge, SIP: <a href="sip:json-ld@digitalbazaar.com">json-ld@digitalbazaar.com</a>.</li>
+                  <li><strong>IRC:</strong> <a href="irc://freenode.net/#json-ld">irc://freenode.net/#json-ld</a>
+                  <li><strong>Duration:</strong> 60 minutes
+               </ul>
+
+               <p>Make sure you have a good headset with a microphone as any background noise is distracting to others during the call. If there is excessive noise on your connection, you will be muted until you need to speak. If you cannot get SIP to work for you, there is an emergency dial-in number. If you use this number regularly, you will be expected to re-imburse the group for call charges. SIP is free for both the caller and the callee - use it. Emergency dial-in number: +1.540.961.4469 x6303.
+               
                <h1>Text and Audio Logs</h1>
                <p>The JSON-LD teleconferences (minutes available as text and audio):</p>
                <ul>
