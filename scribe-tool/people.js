431c4c8b15cca3423c94b4460c17146d2cb5c897
Manu Sporny <msporny@digitalbazaar.com>
2011-09-10 02:15:15 -0400

    Fixed minor scrawl.js bugs and made it more modular.

---
+++ scribe-tool/people.js
@@ -0,0 +1,84 @@
+(function($)
+{
+   window.scrawl = window.scrawl || {};
+   var scrawl = window.scrawl;
+
+   var people = 
+   {
+      "Bradley Allen":
+      {
+         "alias": ["ballen"],
+         "homepage": "http://bradleypallen.org/about"
+      },
+      "Nicolas Dufour":
+      {
+         "alias": ["capnemo"],
+         "homepage": "http://www.nemoworld.info/"
+      },
+      "Danny Ayers":
+      {
+         "alias": ["danja"],
+         "homepage": "http://dannyayers.com/"
+      },
+      "Michael Johnson":
+      {
+         "alias": ["mjohnson", "mjohnson_db"]
+      },
+      "Gregg Kellogg":
+      {
+         "alias": "gkellogg",
+         "homepage": "http://greggkellogg.net/pages/about"
+      },
+      "Markus Lanthaler":
+      {
+         "alias": ["MarkusLanthaler"],
+         "homepage": "https://twitter.com/markuslanthaler"
+      },
+      "Niklas Lindström":
+      {
+         "alias": ["lindstream", "niklasl"]
+      },
+      "David I. Lehn":
+      {
+         "alias": ["dil", "dlehn", "taaz"],
+         "homepage": "http://dil.lehn.org/"
+      },
+      "Dave Longley":
+      {
+         "alias": ["dlongley", "dlongley-db"]
+      },
+      "Alfonso Martin":
+      {
+         "alias": ["amartin"]
+      },
+      "Alexandre Passant":
+      {
+         "alias": ["terraces"],
+         "homepage": "http://apassant.net/"
+      },
+      "Nathan Rixham":
+      {
+         "alias": ["webr3"],
+         "homepage": "http://webr3.org/"
+      },
+      "Manu Sporny":
+      {
+         "alias": ["manu-db", "manu1", "manu`"],
+         "homepage": "http://manu.sporny.org/about"
+      },
+      "Thomas Steiner":
+      {
+         "alias": ["tomayac"],
+         "homepage": "https://plus.google.com/103733874714446059779/about"
+      },
+      "Ted Thibodeau Jr.":
+      {
+         "alias": ["macted"],
+         "homepage": "http://www.linkedin.com/in/macted"
+      }
+   };
+
+   scrawl.people = people;
+
+})(jQuery);
+
