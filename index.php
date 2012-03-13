196bdc6e5c0c3cc112c339a3bd52182bb90e81e6
Manu Sporny <msporny@digitalbazaar.com>
2012-03-13 16:41:23 -0400

    Summarize all resolutions on minutes/ page and predict next meeting date.

--- index.php
+++ index.php
@@ -33,32 +33,80 @@ print <<< htmlcode
                <p>All JSON-LD teleconferences are open to the public. Anyone may join and participate in the discussion. All teleconferences are announced at least 24 hours in advance on the <a href="http://lists.w3.org/Archives/Public/public-linked-json/">JSON-LD mailing list</a>.</p>
 
                <ul>
-                  <li><strong>When:</strong> Every Tuesday</li>
+                  <li><strong>Next Meeting:</strong> 
+htmlcode;
+
+print date('l, F jS (Y-m-d)', strtotime('next Tuesday'));
+
+print <<< htmlcode
+                  </li>
                   <li><strong>Time:</strong> 1500 UTC, 7am San Francisco, 10am Boston, 3pm London</li>
-                  <li><strong>Where:</strong> Digital Bazaar JSON-LD Telecon Bridge, SIP: <a href="sip:json-ld@digitalbazaar.com">json-ld@digitalbazaar.com</a>.</li>
+                  <li><strong>Where:</strong> Digital Bazaar JSON-LD Telecon Bridge, SIP: <a href="sip:json-ld@digitalbazaar.com">json-ld@digitalbazaar.com</a>, tel: +1.540.961.4469 x6303.</li>
                   <li><strong>IRC:</strong> <a href="irc://freenode.net/#json-ld">irc://freenode.net/#json-ld</a>
                   <li><strong>Duration:</strong> 60 minutes
                </ul>
 
-               <p>Make sure you have a good headset with a microphone as any background noise is distracting to others during the call. If there is excessive noise on your connection, you will be muted until you need to speak. If you cannot get SIP to work for you, there is an emergency dial-in number. If you use this number regularly, you will be expected to re-imburse the group for call charges. SIP is free for both the caller and the callee - use it. Emergency dial-in number: +1.540.961.4469 x6303.
+               <p>Make sure you have a good headset with a microphone as any background noise is distracting to others during the call. If there is excessive noise on your connection, you will be muted until you need to speak. Make sure you join the IRC channel as links and code examples are usually shared over the chat channel.</p>
                
                <h1>Text and Audio Logs</h1>
-               <p>The JSON-LD teleconferences (minutes available as text and audio):</p>
-               <ul>
+               <p>The public JSON-LD teleconferences and all of the decisions made in those meetings are listed below. Click on each link to take you to the full text and audio log of the meeting:</p> 
 htmlcode;
 
-$allMinutes = scandir('.');
+// Generate the minutes summary cache
+$mscfilename = "minutes-summary-cache.html";
+$mtime = filemtime($mscfilename);
 
-foreach($allMinutes as $minutes)
+// refresh the cache on demand, every hour
+$mcache = fopen($mscfilename, "c+");
+if(!file_exists($mscfilename) or ((time() - $mtime) > 3600))
 {
-   if(preg_match("/201[0-9]-[0-9]{2,2}-[0-9]{2,2}/", $minutes))
+   $allMinutes = array_reverse(scandir('.'));
+   fwrite($mcache, "<ul>\n");
+   foreach($allMinutes as $minutes)
    {
-     print("                 <li><a href=\"$minutes/\">$minutes</a></li>");
+      if(preg_match("/201[0-9]-[0-9]{2,2}-[0-9]{2,2}/", $minutes))
+      {
+         fwrite($mcache, "   <li><a href=\"$minutes/\">Text and Audio Minutes for  $minutes</a>\n");
+
+         // open the IRC log file
+         $irclogfilename = $minutes . "/irc.log";
+         $irclog = file($irclogfilename, 
+            FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+         $resolution = 1;
+
+         // process the raw IRC log and output the resolutions
+         fwrite($mcache, "      <ol>\n");
+         foreach($irclog as $line_num => $line)
+         {
+            if(preg_match("/.*RESOLVED: (.*)/", $line, $matches))
+            {
+               // link to each resolution in the HTML minutes
+               fwrite($mcache, 
+                  "         <li>" .
+                  htmlspecialchars($matches[1]) . 
+                  " [<a href=\"$minutes/#resolution-$resolution\">" .
+                  "permalink</a>]</li>\n");
+               $resolution += 1;
+            }
+         }
+         fclose($irclog);
+
+         fwrite($mcache, "      </ol></li>");
+      }
    }
+   fwrite($mcache, "</ul>\n");
+   
+   fseek($mcache, 0);
+   print fread($mcache, 65536);
 }
+else
+{
+   print fread($mcache, 65536);
+}
+fclose($mcache);
 
 print <<< htmlcode
-               </ul>
+               </p>
 
                <h1>Tools</h1>
                <ul>
