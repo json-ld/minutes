7b9bef39c77b96339b7b72415c9e0a20e558acaf
Benjamin Young <byoung@digitalbazaar.com>
2025-09-25 09:51:58 -0400

    Use latest Scrawl.js in place of local fork.

--- scribe-tool/generate_index.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Generates minutes for the directory given
-
-echo "Generating minutes index"
-
-node index.js -m -i
--- scribe-tool/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Generates minutes for the directory given
-
-echo "Generating minutes for $1"
-DATESTR=`echo $1 | cut -f2 -d/`
-MESSAGE="Add text minutes $DATESTR telecon."
-
-node index.js -d $1 -m -i
